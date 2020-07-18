bdcbe9ba593e0e77bd8f851fff7410b3a1f3cb00
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-07-18 19:20:00 -0300

    Updates

---
+++ precache-manifest.0fc63b6483b10b22380fcd055ae0941c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e350708eae80b895068e985c19682ef",
+    "url": "/pomodorr/index.html"
+  },
+  {
+    "revision": "4ba615cf4364fe5b501c",
+    "url": "/pomodorr/static/css/main.b0628948.chunk.css"
+  },
+  {
+    "revision": "9581b22e29fb5196be68",
+    "url": "/pomodorr/static/js/2.41534c91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodorr/static/js/2.41534c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba615cf4364fe5b501c",
+    "url": "/pomodorr/static/js/main.e4802309.chunk.js"
+  },
+  {
+    "revision": "3a78ccbfc938da421163",
+    "url": "/pomodorr/static/js/runtime-main.d71fc3a1.js"
+  }
+]);
